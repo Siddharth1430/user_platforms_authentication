5ef086348aad28438b121f10ed5194862f5edf2c
Siddharth <sid.c.287@gmail.com>
2025-02-27 17:25:19 +0530

    Add router in main for user_routes

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.admin.admin_routes import router as admin_router
 from app.api.signup.register_route import router as register_router
 from app.api.signup.login_route import router as login_router
+from app.api.user.user_routes import router as user_router
 
 
 app = FastAPI()
@@ -10,9 +11,4 @@ app = FastAPI()
 app.include_router(admin_router)
 app.include_router(register_router)
 app.include_router(login_router)
-
-
-# /users/{userid} -> should return user details
-# /users/{userid}/platforms -> should return all platforms user integrated
-# /users/{userid}/{platformid}/credentials -> /platforms/{platformid}/credentials?userid==id
-# /platforms/{platformid} -> should return platform details
+app.include_router(user_router)
